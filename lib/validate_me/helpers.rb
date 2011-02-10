61dba368470b712affcd798b62970c5b2c7168ac
Joe Corcoran <joe@tribesports.com>
2011-02-10 23:36:13 +0000

    .gitignore and shorter helper

--- lib/validate_me/helpers.rb
+++ lib/validate_me/helpers.rb
@@ -5,10 +5,9 @@ module ValidateMe
     include ActionView::Helpers::TagHelper
     
     def validated_text_field(method, options = {})
-      object_name = self.object_name
       validators = self.object.class.validators_on(method).collect{ |v| { :kind => v.kind.to_s, :options => v.options } }
       options = { "data-validate" => validators.to_json }.merge(options)
-      @template.text_field(object_name, method, options)
+      @template.text_field(self.object_name, method, options)
     end
 
   end
