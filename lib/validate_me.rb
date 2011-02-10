e6e6ce2a2e039b8e2bba5a05cda0c2fee1b9caa3
Joe Corcoran <joe@tribesports.com>
2011-02-10 23:43:28 +0000

    trying include instead of extend

--- lib/validate_me.rb
+++ lib/validate_me.rb
@@ -1,3 +1,3 @@
 require File.dirname(__FILE__) + '/validate_me/helpers'
 
-ActionView::Helpers::FormBuilder.extend(ValidateMe::Helpers)
+::ActionView::Helpers::FormBuilder.send(:include, ValidateMe::Helpers)
