445cf93c4806780b30938644556c5a9362879fbf
Joe Corcoran <joe@tribesports.com>
2011-03-07 13:27:53 +0000

    Fix generator options

--- lib/generators/validate_me/validate_me_generator.rb
+++ lib/generators/validate_me/validate_me_generator.rb
@@ -1,8 +1,11 @@
-class ValidateMeGenerator < Rails::Generators::NamedBase
+class ValidateMeGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
+  argument :path, :type => :string, :default => "public/javascripts"
+  class_option :example, :type => :boolean, :default => true, :description => "Copy example jQuery element bindings"
  
   desc "This copies the validate_me jQuery plugin to your public/javascripts directory"
   def copy_initializer_file
-    copy_file "jquery.validateMe.js", "public/javascripts/jquery.validateMe.js"
+    copy_file "jquery.validateMe.js", "#{path}/jquery.validateMe.js"
+    copy_file "example.validateMe.js", "#{path}/example.validateMe.js" if options.example?
   end
 end
