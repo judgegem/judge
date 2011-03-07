fac0a02bd1279b0362718986f3eca7f9f122ed01
Joe Corcoran <joe@tribesports.com>
2011-03-07 13:34:24 +0000

    Nicer description

--- lib/generators/validate_me/validate_me_generator.rb
+++ lib/generators/validate_me/validate_me_generator.rb
@@ -3,7 +3,15 @@ class ValidateMeGenerator < Rails::Generators::Base
   argument :path, :type => :string, :default => "public/javascripts"
   class_option :example, :type => :boolean, :default => true, :description => "Copy example jQuery element bindings"
  
-  desc "This copies the validate_me jQuery plugin to your public/javascripts directory"
+  desc %Q{
+    This copies the validate_me jQuery plugin to your application.
+    The default path is public/javascripts, but you can set your own as follows:
+
+      rails g validate_me my/super/cool/path
+
+    Some example element bindings are also copied into this directory.
+    If you don't want them, use the --skip-example option.
+  }
   def copy_initializer_file
     copy_file "jquery.validateMe.js", "#{path}/jquery.validateMe.js"
     copy_file "example.validateMe.js", "#{path}/example.validateMe.js" if options.example?
