1fd22801f161271b6b89dc8802b0e2363ad1c821
Joe Corcoran <joecorcoran@gmail.com>
2011-07-17 22:54:32 +0100

    change generator

--- lib/generators/judge/judge_generator.rb
+++ lib/generators/judge/judge_generator.rb
@@ -1,19 +1,14 @@
 class JudgeGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
   argument :path, :type => :string, :default => "public/javascripts"
-  class_option :example, :type => :boolean, :default => true, :description => "Copy example jQuery element bindings"
  
   desc %Q{
-    This copies the judge jQuery plugin to your application.
+    This copies the judge JavaScript file to your application.
     The default path is public/javascripts, but you can set your own as follows:
 
-      rails g judge my/super/cool/path
-
-    Some example element bindings are also copied into this directory.
-    If you don't want them, use the --skip-example option.
+      rails generate judge my/super/cool/path
   }
   def copy_files
-    copy_file "jquery.judge.js", "#{path}/jquery.judge.js"
-    copy_file "example.judge.js", "#{path}/example.judge.js" if options.example?
+    copy_file "judge.js", "#{path}/judge.js"
   end
 end
