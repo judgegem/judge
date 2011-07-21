536155994357901bfab24fe2134733629e3c3545
Joe Corcoran <joecorcoran@gmail.com>
2011-07-21 13:20:28 +0100

    change desc in generator

--- lib/generators/judge/judge_generator.rb
+++ lib/generators/judge/judge_generator.rb
@@ -1,15 +1,15 @@
 class JudgeGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
   argument :path, :type => :string, :default => "public/javascripts"
-  class_option :dep, :type => :boolean, :default => true, :description => "Copy underscore.js and json2.js"
+  class_option :dep, :type => :boolean, :default => true, :desc => "Copy underscore.js and json2.js"
  
   desc %Q{
-    This copies judge.js (and dependencies) to your application.
-    The default path is public/javascripts, but you can set your own as follows:
+This copies judge.js (and dependencies) to your application.
+The default path is public/javascripts, but you can set your own as follows:
 
-      rails generate judge my/super/cool/path
+  rails generate judge my/super/cool/path
 
-    Use the --no-dep option if you don't want to copy underscore.js and json2.js too.
+Use the --no-dep option if you don't want to copy underscore.js and json2.js too.
   }
   def copy_files
     copy_file "judge.js", "#{path}/judge.js"
