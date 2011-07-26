7fa0705e237ca7d37e8be1383d7d8aee76604cd4
Joe Corcoran <joecorcoran@gmail.com>
2011-07-26 12:51:09 +0100

    nope, going back to normal module patching

--- lib/judge.rb
+++ lib/judge.rb
@@ -2,4 +2,7 @@ require File.dirname(__FILE__) + '/judge/utils'
 require File.dirname(__FILE__) + '/judge/form'
 
 ::ActionView::Helpers::FormBuilder.send(:include, Judge::FormBuilder)
-::ApplicationHelper.send(:include, Judge::FormHelper)
+
+module ApplicationHelper
+  include Judge::FormHelper
+end
