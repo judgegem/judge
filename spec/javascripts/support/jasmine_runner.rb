63fac7ce01871ace7ad9c694cfb49cc1c2c8bb9d
Joe Corcoran <joecorcoran@gmail.com>
2012-03-06 19:02:34 +0000

    fix jasmine_runner.rb

--- spec/javascripts/support/jasmine_runner.rb
+++ spec/javascripts/support/jasmine_runner.rb
@@ -4,27 +4,16 @@ require 'rubygems'
 require 'jasmine'
 jasmine_config_overrides = File.expand_path(File.join(File.dirname(__FILE__), 'jasmine_config.rb'))
 require jasmine_config_overrides if File.exist?(jasmine_config_overrides)
-if Jasmine::rspec2?
-  require 'rspec'
-else
-  require 'spec'
-end
+require 'rspec'
+
 
 jasmine_config = Jasmine::Config.new
 spec_builder = Jasmine::SpecBuilder.new(jasmine_config)
 
 should_stop = false
 
-if Jasmine::rspec2?
-  RSpec.configuration.after(:suite) do
-    spec_builder.stop if should_stop
-  end
-else
-  Spec::Runner.configure do |config|
-    config.after(:suite) do
-      spec_builder.stop if should_stop
-    end
-  end
+RSpec.configuration.after(:suite) do
+  spec_builder.stop if should_stop
 end
 
 spec_builder.start
