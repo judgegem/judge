9af2bb4fb961f00558f59e17b4c9d3470d603f3c
Joe Corcoran <joecorcoran@gmail.com>
2012-04-04 23:41:09 +0100

    generate messages in a slightly more sane way

--- lib/judge/message_collection.rb
+++ lib/judge/message_collection.rb
@@ -9,21 +9,17 @@ module Judge
     attr_reader   :object, :method, :kind, :options
     attr_accessor :messages
 
-    def initialize(object, method, amv, opts = {})
-      opts      = { :generate => true }.merge(opts)
+    def initialize(object, method, amv)
       @object   = object
       @method   = method
       @kind     = amv.kind
       @options  = amv.options.dup
-      @messages = {}
-      generate_messages! unless opts[:generate] == false
+      @messages = generate_messages
     end
 
-    def generate_messages!
-      if messages.blank?
-        %w{base options blank integer}.each do |type|
-          self.send(:"generate_#{type}!")
-        end
+    def generate_messages
+      %w{base options blank integer}.inject({}) do |messages, type|
+        messages.merge(self.send(:"generate_#{type}"))
       end
     end
 
@@ -33,14 +29,17 @@ module Judge
 
     private
 
-    def generate_base!
+    def generate_base
+      messages = {}
       if MESSAGE_MAP.has_key?(kind) && MESSAGE_MAP[kind][:base].present?
         base_message = MESSAGE_MAP[kind][:base]
         messages[base_message] = object.errors.generate_message(method, base_message, options)
       end
+      messages
     end
 
-    def generate_options!
+    def generate_options
+      messages = {}
       if MESSAGE_MAP.has_key?(kind) && MESSAGE_MAP[kind][:options].present?
         opt_messages = MESSAGE_MAP[kind][:options]
         opt_messages.each do |opt, opt_message|
@@ -50,18 +49,23 @@ module Judge
           end
         end
       end
+      messages
     end
 
-    def generate_blank!
+    def generate_blank
+      messages = {}
       if ALLOW_BLANK.include?(kind) && options[:allow_blank].blank? && messages[:blank].blank?
         messages[:blank] = object.errors.generate_message(method, :blank)
       end
+      messages
     end
 
-    def generate_integer!
+    def generate_integer
+      messages = {}
       if kind == :numericality && options[:only_integer].present?
         messages[:not_an_integer] = object.errors.generate_message(method, :not_an_integer)
       end
+      messages
     end
 
   end
