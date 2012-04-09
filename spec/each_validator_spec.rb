a8dcb25d233f79e809b6ce31852222c5dc86709d
Joe Corcoran <joecorcoran@gmail.com>
2012-04-09 13:04:52 +0100

    check custom validator includes Judge::EachValidator

--- spec/each_validator_spec.rb
+++ spec/each_validator_spec.rb
@@ -4,6 +4,10 @@ describe Judge::EachValidator do
 
   let(:amv) { User.validators_on(:foo).first }
 
+  specify "custom validators include Judge::EachValidator" do
+    FooValidator.include?(Judge::EachValidator).should be_true
+  end
+
   specify "#messages_to_lookup method should return array of messages" do
     amv.should respond_to :messages_to_lookup
     amv.messages_to_lookup.should be_an Array
