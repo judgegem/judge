f94a4cfdc08b62e10cd161a975e6b77694bc2004
Yann IRBAH <yirbah@siliconsalad.com>
2012-04-18 15:17:46 +0200

    Manage case when validating format against a regexp using the characters '\A' and '\z' not supported in Javascript : - Replaces '\A' with ^ and '\z' with $. Not the same meaning but enough for client-side validation

--- lib/judge/validator.rb
+++ lib/judge/validator.rb
@@ -6,7 +6,7 @@ module Judge
 
     def initialize(object, method, amv)
       @kind     = amv.kind
-      @options  = amv.options.reject { |key| [:if, :on, :unless, :tokenizer].include?(key)  }
+      @options  = set_options(amv.options)
       @method   = method
       @messages = Judge::MessageCollection.new(object, method, amv)
     end
@@ -17,6 +17,30 @@ module Judge
         :options => options,
         :messages => messages.to_hash
       }
+    end
+
+  private
+
+    def set_options(options)
+      options     = options.reject { |key| [:if, :on, :unless, :tokenizer].include?(key)  }
+      method_name = "get_js_compatible_#{self.kind}"
+      if self.respond_to? method_name, options
+        options = self.send method_name, options
+      end
+      options
+    end
+
+    def get_js_compatible_format(options)
+      format_options = Hash.new
+      options.each do |key, option|
+        format_options[key] = (key == :with) ? convert_regex_to_js_compatible(option) : option
+      end
+      format_options
+    end
+
+    def convert_regex_to_js_compatible(regex)
+      js_regex_source = regex.source.sub(/^\\A/, '^').sub(/\\z$/i, '$')
+      /#{js_regex_source}/
     end
 
   end
