062423554cb11ad99f528962ae4cbf7874deaf03
Joe Corcoran <joecorcoran@gmail.com>
2012-05-01 18:27:27 +0100

    remove comment from dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -6,7 +6,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 Bundler.require
 require "judge"
