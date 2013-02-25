dadd877b22ef22f76b67e2f04d24ca4c8b06df42
Joe Corcoran <joecorcoran@gmail.com>
2013-02-25 17:23:26 +0000

    Fix up generator for apps without asset pipeline
    No longer need to worry about 3.0 in here. Also fixes the source_root so we can
    actually find and copy the assets.

--- lib/generators/judge/install/install_generator.rb
+++ lib/generators/judge/install/install_generator.rb
@@ -2,26 +2,26 @@ module Judge
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
 
-      desc %Q{For use in Rails 3.0 apps or more recent apps with asset pipeline disabled.
+      desc %Q{For use where asset pipeline is disabled.
 Installs judge.js and optionally installs underscore.js and json2.js.
 
 Copy judge.js to public/javascripts:
   $ rails generate judge:install
 
 Copy judge.js to different path:
-  $ rails generate judge:install path/to/javascript/dir
+  $ rails generate judge:install path
 
 Copy judge.js and dependencies:
-  $ rails generate judge:install --underscore --json2
+  $ rails generate judge:install path --underscore --json2
 }
 
       argument :path, :type => :string, :default => "public/javascripts"
       class_option :underscore, :type => :boolean, :default => false, :desc => "Install underscore.js"
       class_option :json2, :type => :boolean, :default => false, :desc => "Install json2.js"
-      source_root File.expand_path("../../..", __FILE__)
+      source_root File.expand_path("../../../../..", __FILE__)
 
       def exec
-        unless ::Rails.version < "3.1" || !::Rails.application.config.assets.enabled
+        unless !::Rails.application.config.assets.enabled
           say_status("deprecated", "You don't need to use this generator as your app is running on Rails >= 3.1 with the asset pipeline enabled")
           return
         end
