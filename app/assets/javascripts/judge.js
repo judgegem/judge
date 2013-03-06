da79f4f4a097ad023ad2f76394d2426f4e1e2475
Joe Corcoran <joecorcoran@gmail.com>
2013-03-06 13:35:22 +0000

    Bump to v2.0.3

--- app/assets/javascripts/judge.js
+++ app/assets/javascripts/judge.js
@@ -1,4 +1,4 @@
-// Judge 2.0.2
+// Judge 2.0.3
 // (c) 2011-2012 Joe Corcoran
 // http://raw.github.com/joecorcoran/judge/master/LICENSE.txt
 
@@ -14,7 +14,7 @@
   var judge = root.judge = {},
       _     = root._;
 
-  judge.VERSION = '2.0.2';
+  judge.VERSION = '2.0.3';
 
   // Trying to be a bit more descriptive than the basic error types allow.
   var DependencyError = function(message) {
--- lib/judge/version.rb
+++ lib/judge/version.rb
@@ -1,3 +1,3 @@
 module Judge
-  VERSION = "2.0.2"
+  VERSION = "2.0.3"
 end
