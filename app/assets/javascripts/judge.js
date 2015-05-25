4567d74cc0f34b5d42ec0d6292075e11412ca415
James Kurczodyna <james@wearefine.com>
2015-05-25 14:45:42 -0700

    updates version number and adds changelog items

--- app/assets/javascripts/judge.js
+++ app/assets/javascripts/judge.js
@@ -1,4 +1,4 @@
-// Judge 2.0.6
+// Judge 2.1.0
 // (c) 2011-2013 Joe Corcoran
 // http://raw.github.com/joecorcoran/judge/master/LICENSE.txt
 
@@ -14,7 +14,7 @@
   var judge = root.judge = {},
       _     = root._;
 
-  judge.VERSION = '2.0.6';
+  judge.VERSION = '2.1.0';
 
   // Trying to be a bit more descriptive than the basic error types allow.
   var DependencyError = function(message) {
